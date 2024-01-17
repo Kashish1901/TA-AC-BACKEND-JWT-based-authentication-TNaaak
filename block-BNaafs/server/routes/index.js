c7cca3e1995dfccaa34a197e01716a7f52da9070
KASHISH <kashu80525@gmail.com>
2024-01-17 22:32:37 +0530

    adding codes

---
+++ block-BNaafs/server/routes/index.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var router = express.Router();
+var auth = require("../middelwares/auth");
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/dashboard", auth.verifyToken, (req, res) => {
+  res.json({ access: "protected resource" });
+});
+module.exports = router;
